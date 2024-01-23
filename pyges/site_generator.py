a56ba08f238c63f868f698bb3f3566617a2f12d9
Ariel Alon <arielalon@Ariels-MacBook-Pro-2.local>
2024-01-23 11:17:39 +0200

    Added back submodule.

--- pyges/site_generator.py
+++ pyges/site_generator.py
@@ -3,6 +3,7 @@ from .typed_template import Template
 
 class SiteGenerator:
     def __init__(self) -> None:
+        self.template = Template("a")
         pass
 
     def generate(self, content: str, out: str) -> None:
