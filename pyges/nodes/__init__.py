7f7f464bbb1b84945d46ee0d78e61473c0ec1b56
Ariel Alon <arielalon@Ariels-MacBook-Pro-2.local>
2024-02-06 10:00:44 +0200

    Added Meta to nodes module.

--- pyges/nodes/__init__.py
+++ pyges/nodes/__init__.py
@@ -4,5 +4,5 @@ from .multimedia import Img
 from .text import H, P, Span, B, Br, I, Strong, Em, Mark, Small, Del, Ins, Sub, Sup
 from .sectioning import Div, Article
 from .attributes import Attribute
-from .meta_links import Link, Nav, A
+from .meta_links import Link, Nav, A, Meta
 from .markdown import Markdown, Scheme
